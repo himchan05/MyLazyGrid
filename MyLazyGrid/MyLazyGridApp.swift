d81380c409930d0cd9d5cbb9ce3507562a8c4bbf
박힘찬 <himchan05@naver.com>
2022-03-19 14:29:15 +0900

    Initial Commit

---
+++ MyLazyGrid/MyLazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLazyGridApp.swift
+//  MyLazyGrid
+//
+//  Created by 박힘찬 on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct MyLazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
